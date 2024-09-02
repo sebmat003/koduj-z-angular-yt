0c8da83e48a9d4bde1701d7bc0b37edec5b0f216
sebmat003 <sebmat003@utp.edu.pl>
2024-09-02 10:08:52 +0200

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Example1Component } from './components/example-1/example-1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, Example1Component],
 })
-export class AppComponent {
-  title = 'loader-directive';
-}
+export class AppComponent {}
--- src/app/components/loader/loader.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loader',
-  template: `<div class="spinner"></div>`,
-  standalone: true,
-  styleUrl: './loader.component.scss',
-})
-export class LoaderComponent {}
